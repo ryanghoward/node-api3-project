e6524a32c533e4e99ec6a1e9745dfd30f7e1b90d
Ryan Howard <ryanghoward@protonmail.com>
2022-02-09 14:16:23 -0800

    Save 2

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,34 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timeStamp}] ${method} to ${url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUserId middleware");
+  next();
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validateUser middleware");
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log("validatePost middleware");
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  validateUserId,
+  validateUser,
+  validatePost,
+  logger,
+};
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
-
+const { logger } = require("./middleware/middleware");
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
 
 server.use(express.json());
+
+server.use(logger);
 
 // global middlewares and the user's router need to be connected here
 
